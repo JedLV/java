1b1010cbb2543779fc78c98c52076aee37e1ec97
编程 <1660873558@qq.com>
2021-07-06 20:20:51 +0800

    第三次提交

---
+++ untitled/src/main/java.java
@@ -0,0 +1,14 @@
+public class j203
+{
+    public  static void main(String args[])
+    {
+        Systerm.out.println("转换成十进制的a=" +a);
+        Systerm.out.println("b=" +b);
+        Systerm.out.println("c=" +c);
+        Systerm.out.println("d=" +c);
+        Systerm.out.println("d=" +d);
+        Systerm.out.println("e=" +e);
+        Systerm.out.println("f=" +f);
+
+    }
+}
